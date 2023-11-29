8740bb8a598ca397627f7670816619cfbd46f2fd
Daniil Satnichenka <daniil.satnichenka@gmail.com>
2023-11-29 16:33:30 +0100

    - added changes to repositories

--- src/main/java/com/mycompany/currencyexchangeservice/repository/SpendingLimitRepository.java
+++ src/main/java/com/mycompany/currencyexchangeservice/repository/SpendingLimitRepository.java
@@ -1,8 +1,12 @@
 package com.mycompany.currencyexchangeservice.repository;
 
+import com.mycompany.currencyexchangeservice.enums.ExpenseCategory;
 import com.mycompany.currencyexchangeservice.model.SpendingLimit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SpendingLimitRepository extends JpaRepository<SpendingLimit, Long> {
-    // Методы для работы с лимитами расходов
+    List<SpendingLimit> findByExpenseCategory(ExpenseCategory expenseCategory);
+
 }
--- src/main/java/com/mycompany/currencyexchangeservice/repository/TransactionRepository.java
+++ src/main/java/com/mycompany/currencyexchangeservice/repository/TransactionRepository.java
@@ -2,10 +2,17 @@ package com.mycompany.currencyexchangeservice.repository;
 
 import com.mycompany.currencyexchangeservice.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    List<Transaction> findByLimitExceededTrueAndTransactionDateBetween(LocalDate start, LocalDate end);
+    List<Transaction> findByLimitExceeded(Boolean limitExceeded);
+
+    // Пример запроса для поиска транзакций с превышением лимита,
+    // вам нужно будет адаптировать его под вашу логику и структуру данных
+    @Query("SELECT t FROM Transaction t WHERE t.limitExceeded = true")
+    List<Transaction> findAllTransactionsExceedingLimit();
+
 }
